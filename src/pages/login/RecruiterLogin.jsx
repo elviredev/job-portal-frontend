05b1f94509677ac174d1a9e48bbaffe7fe7e81ca
elviredev <sandrine.rodriguez@hotmail.com>
2026-06-17 14:02:52 +0200

    🚀 Mise en place Google Login et Manual Login pour le Recruteur.

--- src/pages/login/RecruiterLogin.jsx
+++ src/pages/login/RecruiterLogin.jsx
@@ -1,9 +1,98 @@
 import { TextInput } from "@/components"
-import { NavLink } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom"
 import { GoogleLogin } from "@react-oauth/google"
+import { useState } from "react"
+import { toast } from "react-toastify"
+import api from "@/api/axios"
+
+const API_URL = import.meta.env.VITE_API_URL
+if (!API_URL) {
+   throw new Error("VITE_API_URL is not defined")
+}
 
 
 const RecruiterLogin = () => {
+
+   const navigate = useNavigate()
+
+   const [loading, setLoading] = useState(false)
+   const [formData, setFormData] = useState({
+      email: "",
+      password: ""
+   })
+
+   const handleChange = (e) => {
+      const { name, value } = e.target
+      setFormData((prev) => ({
+         ...prev,
+         [name]: value
+      }))
+   }
+
+   // Google Login
+   const handleSuccess = async (credentialResponse) => {
+      try {
+         setLoading(true)
+
+         const token = credentialResponse.credential
+         await api.post(`${API_URL}/auth/google-login`, {
+            token,
+            role: "recruiter"
+         },
+            {
+               withCredentials: true // http-only cookie
+            }
+         )
+
+         // after login success
+         toast.success('Google logged successfully!')
+         navigate('/')
+      } catch (error) {
+         toast.error(error.response?.data?.error || 'Google login failed')
+      } finally {
+         setLoading(false)
+      }
+   }
+
+   const handleError = () => {
+      console.log("Google login failed");
+      toast.error("Google login failed, please try again!")
+   }
+
+   // Manual Login
+   const handleManualLogin = async (e) => {
+      e.preventDefault()
+
+      try {
+         setLoading(true)
+
+         await api.post(`${API_URL}/auth/login`,
+            {
+               email: formData.email,
+               password: formData.password,
+               role: "recruiter"
+            },
+            {
+               withCredentials: true // http-only cookie
+            }
+         )
+
+         // after login success
+         toast.success('You are logged successfully!')
+         navigate('/')
+      } catch (error) {
+         const message = 
+            error.response?.data?.error || 
+            error.response?.data?.message || 
+            error.message ||
+            "Login failed"
+         toast.error(message)
+      } finally {
+         setLoading(false)
+      }
+   }
+
+
    return (
       <div className="grow flex justify-center items-center p-6">
          <div className="bg-white rounded-2xl shadow-xl w-full max-w-md border border-gray-100">
@@ -20,14 +109,12 @@ const RecruiterLogin = () => {
                {/* Google Login */}
                <div className="mb-6">
                   <div className="border border-gray-300 rounded-lg overflow-hidden">
-                     <GoogleLogin
-                        onSuccess={(credentialResponse) => {
-                           console.log('SUCCESS', credentialResponse)
-                        }}
-                        onError={() => {
-                           console.log('ERROR')
-                        }}
-                     />
+                     {!loading && (
+                        <GoogleLogin
+                           onSuccess={handleSuccess}
+                           onError={handleError}
+                        />
+                     )}
                   </div>
                </div>
 
@@ -39,7 +126,7 @@ const RecruiterLogin = () => {
                </div>
 
                {/* Manual Login Form */}
-               <form className="space-y-5">
+               <form onSubmit={handleManualLogin} className="space-y-5">
 
                   {/* Email */}
 
@@ -47,6 +134,8 @@ const RecruiterLogin = () => {
                      label="Email Address"
                      type="email"
                      name="email"
+                     value={formData.email}
+                     onChange={handleChange}
                      placeholder="Enter your email"
                      required
                   />
@@ -56,6 +145,8 @@ const RecruiterLogin = () => {
                      label="Password"
                      type="password"
                      name="password"
+                     value={formData.password}
+                     onChange={handleChange}
                      placeholder="Enter your password"
                      required
                   />
@@ -64,9 +155,14 @@ const RecruiterLogin = () => {
 
                   <button
                      type="submit"
-                     className="w-full bg-linear-to-r from-purple-600 to-indigo-600 hover:from-purple-700 hover:to-indigo-700 text-white py-3 rounded-xl font-semibold transition duration-200 shadow-sm shadow-purple-200"
+                     disabled={loading}
+                     className={`w-full py-3 rounded-xl font-semibold transition duration-200
+                        ${loading
+                           ? "bg-gray-400 cursor-not-allowed"
+                           : "bg-linear-to-r from-purple-600 to-indigo-600 hover:from-purple-700 hover:to-indigo-700 shadow-sm shadow-purple-200"
+                        } text-white `}
                   >
-                     Log In as Recruiter
+                     {loading ? 'Signin In...' : 'Log In as Recruiter'}
                   </button>
                </form>
 
